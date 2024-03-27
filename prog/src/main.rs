6887f447880450a9d42e40a5a9ed11053f2b52d0
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-27 16:00:43 -0400

    Catch up to C in Rust

--- prog/src/main.rs
+++ prog/src/main.rs
@@ -4,13 +4,41 @@ fn main() {
     let mut file = get_file_contents("../../test.pr");
     remove_comments(&mut file);
     let lines = split_file_into_lines(file);
-    for line in lines {
+    for line in &lines {
         let tokens = tokenize(&line);
-        for token in tokens {
-            println!("token: {}", token);
-        }
+        serve_keyword_token(&tokens);
     }
     // let mut test = String::new(); std::io::stdin().read_line(&mut test); // for leaks
+}
+
+fn serve_keyword_token(tokens: &Vec<String>) {
+    let tokens = remove_whitespace_tokens(tokens);
+    if !tokens.is_empty() {
+        match tokens[0].as_str() {
+            "print" => print!("{}", tokens[1]),
+            "puts" => println!("{}", tokens[1]),
+            "let" => println!("I'm in the `let` branch"),
+            "for" => println!("I'm in the `for` branch"),
+            "if" => println!("I'm in the `if` branch"),
+            _ => println!("This line doesn't have a keyword"),
+        }
+    }
+}
+
+fn trim_leading_whitespace(token: &String) -> String {
+    token.trim_start().to_string()
+}
+
+fn remove_whitespace_tokens(tokens: &Vec<String>) -> Vec<String> {
+    let mut new_vec = Vec::<String>::new();
+    for token in tokens {
+        let token = trim_leading_whitespace(&token);
+        if token.is_empty() || token == " " {
+            continue;
+        }
+        new_vec.push(token.to_owned());
+    }
+    new_vec
 }
 
 fn split_file_into_lines(file: String) -> Vec<String> {
@@ -34,25 +62,26 @@ fn contains_valid_string(line: &String) -> bool {
 
 fn tokenize_string(line: &String, start_index: usize) -> String {
     let mut result_string = String::new();
-    for i in start_index..line.len() {
-        let c = {
-            &line
-                .chars()
-                .nth(i)
-                .unwrap_or_default()
-                .to_string()
-        };
-        if c == &'"'.to_string() {
+    for i in start_index..line.len()-2 {
+        let c = &line.chars().nth(i).unwrap();
+        let next = &line.chars().nth(i+1).unwrap();
+        if next == &'"' && c != &'\\' {
             break;
         }
-        result_string += c;
+        if c == &'\\' && next == &'n' {
+            result_string += "\n";
+            continue;
+        }
+        if next != &'\\' {
+            result_string += &next.to_string();
+        }
     }
     result_string
 }
 
 fn tokenize_string_line(tokens: &mut Vec<String>, line: &String) {
     let index = line.find("\"").unwrap();
-    let internal_string = tokenize_string(&line, index + 1);
+    let internal_string = tokenize_string(&line, index);
     let mut token = String::new();
     for c in line.chars() {
         if c == '"' { 
