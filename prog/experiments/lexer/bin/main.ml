3fd0ff85ca1c72d2ce2e1e3057b7e85165af675e
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-27 16:01:01 -0400

    Update root dir

---
+++ prog/experiments/lexer/bin/main.ml
@@ -0,0 +1,18 @@
+(* open Re *)
+
+let path = "../../../test.pr";;
+
+let split_file_into_lines file = 
+    String.split_on_char '\n' file;;
+
+let read_file_to_string path = 
+    let file = open_in_bin path in
+    let str = really_input_string file (in_channel_length file) in
+    close_in file;
+    str;;
+
+let result = read_file_to_string path in
+let lines = split_file_into_lines result in
+List.iter (fun line -> print_endline line) lines
+
+
