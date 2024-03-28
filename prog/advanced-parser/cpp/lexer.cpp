b641dd3e3b35e27428c65494a707cacb39cb83da
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-28 17:02:26 -0400

    Move parameter to return type for token functions

---
+++ prog/advanced-parser/cpp/lexer.cpp
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "lexer.hpp"
+
+using namespace std;
+
+Lexer::Lexer(string text) :
+    _text(text),
+    _current(text.empty() ? '\0' : text[0]), 
+    _index(0), _next(text.size() > 1 ? text[1] : '\0'),
+    _size(text.size()) 
+{}
+
+vector<string> removeEmptyTokens(vector<string> tokens)
+{
+    vector<string> result;
+    for (string token : tokens) {
+        if (!token.empty()) 
+            result.push_back(token);
+    }
+    return result;
+}
+
+string removeComment(string line) 
+{
+    Lexer l (line);
+    string newStr = "";
+    bool isComment = false;
+    while (l.getNext() == '\n' || l.getIndex() < l.size()-1) {
+        if (l.getCurrent() == '/' && l.getNext() == '/')
+            isComment = true;
+        if (!isComment)
+            newStr += l.getCurrent();
+        l.increment();
+    }
+    return newStr;
+}
+
+string trimLeadingWhitespace(string line) 
+{
+    string result = "";
+    size_t i = line.find_first_not_of(' ');
+    if (i != string::npos) {
+        while (i < line.size()) {
+            result += line[i];
+            i++;
+        }
+    }
+    return result;
+}
+
+vector<string> tokenizeLine(string line)
+{
+    Lexer l(line);
+    vector<string> result;
+    if (line.find("\"") != string::npos) 
+        result = tokenizeStringLine(l, line);
+    else 
+        result = tokenizeNonStringLine(l, line);
+    
+    return result;
+}
+
+vector<string> tokenizeNonStringLine(Lexer l, string line)
+{
+    vector<string> result;
+    string token = "";
+    while (true) {
+        if (l.getCurrent() == ' ') {
+            token = trimLeadingWhitespace(token);
+            result.push_back(token);
+            token = "";
+        }
+        token += l.getCurrent();
+        if (l.getNext() == '\n' || l.getIndex() >= l.size()) 
+            goto end;
+        l.increment();
+    }
+    end:
+    token = trimLeadingWhitespace(token);
+    result.push_back(token);
+    return result;
+}
+
+vector<string> tokenizeStringLine(Lexer l, string line) 
+{
+    vector<string> result;
+    string embeddedStr = tokenizeString(line);
+    string token = "";
+    while (true) {
+        if (l.getCurrent() == ' ') {
+            token = trimLeadingWhitespace(token);
+            result.push_back(token);
+            token = "";
+        }
+        token += l.getCurrent();
+        if (l.getCurrent() == '"') {
+            goto end;
+        }
+        l.increment();
+    }
+    end:
+    result.push_back(embeddedStr);
+    return result;
+}
+
+string tokenizeString(string line) 
+{
+    string result = "";
+    Lexer l(line);
+    bool inString = false;
+    while (l.getIndex() < l.size()-1) {
+        if (l.getCurrent() == '"') {
+            if (inString) 
+                result += l.getCurrent();
+            inString = true;
+            l.increment();
+            continue;
+        }
+        if (inString) {
+            if (l.getCurrent() == '\\' && l.getNext() == 'n') {
+                result += '\n';
+                for (int i = 0; i < 2; i++)
+                    l.increment();
+                continue;
+            }
+            if (l.getNext() == '"' && l.getCurrent() != '\\') {
+                result += l.getCurrent();
+                goto end;
+            }
+            result += l.getCurrent();
+        }
+        l.increment();
+    }
+    end:
+    return result;
+}
+
+vector<string> splitIntoLines(string file)
+{
+    vector<string> lines;
+    string line = "";
+    for (char c : file) {
+        line += c;
+        if (c == '\n') {
+            lines.push_back(line);
+            line = "";
+        }
+    }
+    return lines;
+}
+
+void Lexer::increment() 
+{
+    if (this->_index < this->_size) {
+        this->_index++;
+        this->_current = _text[this->_index];
+        this->_next = _text[this->_index+1];
+    }
+}
+char Lexer::getCurrent() { return _current; }
+char Lexer::getNext() { return _next; }
+int Lexer::getIndex() { return _index; }
+int Lexer::size() { return _size; }
+
+string readFile(string path) 
+{
+    ifstream file;
+    string line;
+    string result = "";
+    file.open(path);
+    if (file.is_open()) {
+        while (getline(file, line)) 
+            result += line + "\n";
+        file.close();
+    }
+    else 
+        cout << "Unable to open file." << endl;
+    return result;
+}
---
+++ prog/advanced-parser/cpp/lexer.hpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <fstream>
+
+#ifndef LEXER_H
+#define LEXER_H
+
+class Lexer
+{
+public:
+    /** 
+     * Constructs a new `Lexer` object. 
+     * @param text The string to feed into the Lexer.
+     */
+    Lexer(std::string text);
+
+    /**
+     * Increases the index by 1 and updates the corresponding member elements. 
+     * If the end of the is reached, nothing happens. 
+     */
+    void increment();
+
+    /**
+     * @return The current character of the input text.
+     */
+    char getCurrent();
+
+    /**
+     * @return The next character of the input text. 
+     */
+    char getNext();
+
+    /**
+     * @return The current index.
+     */
+    int getIndex();
+
+    /**
+     * @return The size of the input string. 
+     */
+    int size();
+
+private:
+    std::string _text;
+    char _current;
+    char _next;
+    int _index;
+    int _size;
+};
+
+/**
+ * Removes empty elements from the input vector.
+ * @param tokens The source vector to filter.
+ * @return A new vector with only non-empty elements from the original.
+ */
+std::vector<std::string> removeEmptyTokens(std::vector<std::string> tokens);
+
+/**
+ * Reads the contents of the file specified with `path`.
+ * @param path The path of the text file to read. 
+ *             If no valid file exists at the path, an error message is printed.
+ * @return The contents of the given file as a string. 
+ *         If no valid file exists, an empty string is returned. 
+ */
+std::string readFile(std::string);
+
+/**
+ * Splits the provided string at newlines. 
+ * @param file The input string to split.
+ * @return The input file split at newlines, stored in a vector of strings. 
+ */
+std::vector<std::string> splitIntoLines(std::string);
+
+/**
+ * Extracts the embedded string from inside of an input string. 
+ * @param line The source string to extract the substring from. 
+ * @return The embedded substring extracted from the source.  
+ */
+std::string tokenizeString(std::string);
+
+/**
+ * Converts the source string into tokens based on their content. 
+ * @param line The source string to split.
+ * @return A vector of strings extracted from the source string. 
+ */
+std::vector<std::string> tokenizeLine(std::string);
+
+/**
+ * Helper function for `tokenizeLine`. 
+ * This is called when a line containing a valid string is found. 
+ * @param l Lexer to handle the string traversal.
+ * @param result A reference to a string vector to save the tokens to. 
+ * @param line The source string to split into tokens.
+ */
+std::vector<std::string> tokenizeStringLine(Lexer, std::string);
+
+/**
+ * Helper function for `tokenizeLine`. 
+ * This is called when a line NOT containing a valid string is found. 
+ * @param l Lexer to handle the string traversal.
+ * @param result A reference to a string vector to save the tokens to. 
+ * @param line The source string to split into tokens.
+ */
+std::vector<std::string> tokenizeNonStringLine(Lexer, std::string);
+
+/**
+ * Removes comments from the input string.
+ * @param line The source string to check for comments.
+ * @return a new string with any comments removed.
+ */
+std::string removeComment(std::string);
+
+#endif
