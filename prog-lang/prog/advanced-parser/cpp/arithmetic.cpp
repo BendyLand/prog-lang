53bc8312f2baceaddb096dd8fa42926158c392d3
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-05-20 20:14:35 -0400

    Begin  project

---
+++ prog-lang/prog/advanced-parser/cpp/arithmetic.cpp
@@ -0,0 +1,120 @@
+#include <vector>
+#include <iostream>
+#include "arithmetic.hpp" // includes <variant>
+
+using namespace std;
+
+string reduceInnerExpression(string nestedExpr)
+{
+    bool inner = false;
+    vector<string> pieces;
+    string temp = "";
+    string innerExpr = "";
+    for (int i = 0; i < nestedExpr.size(); i++) {
+        if (inner) {
+            if (nestedExpr[i] == ')') {
+                innerExpr += nestedExpr[i];
+                pieces.push_back(innerExpr);
+                inner = false;
+                continue;
+            }
+            innerExpr += nestedExpr[i];
+            continue;
+        }
+        if (i > 0 && nestedExpr[i] == '(') {
+            inner = true;
+            innerExpr += nestedExpr[i];
+            pieces.push_back(temp);
+            temp = "";
+            continue;
+        }
+        temp += nestedExpr[i];
+    }
+    pieces.push_back(temp);
+    for (auto item : pieces) {
+        cout << "Item: " << item << endl;
+    }
+    return ""; // once pieces[1] is evaluated, the vector can be joined and returned
+}
+
+bool checkForNestedParenthesis(string expr)
+{
+    bool first = false;
+    for (char c : expr) {
+        if (first) {
+            if (c == '(') {
+                return true;
+            }
+        }
+        if (c == '(') {
+            first = true;
+        }
+    }
+    return false;
+}
+
+vector<string> extractExpressionsInParenthesis(string expr)
+{
+    bool inExpr = false;
+    string temp;
+    vector<string> result;
+    size_t count = 0;
+    for (char c : expr) {
+        if (c == '(') {
+            inExpr = true;
+            count++;
+        }
+        if (inExpr) {
+            temp += c;
+        }
+        if (c == ')') {
+            count--;
+            if (count == 0) {
+                result.push_back(temp);
+                temp = "";
+            }
+        }
+        if (c == ')' && count == 0) {
+            inExpr = false;
+        }
+    }
+    return result;
+}
+
+double evaluateExpression(Number n1, Number n2, string op)
+{
+    double num1;
+    double num2;
+    double result;
+    if (holds_alternative<int>(n1)) 
+        num1 = (double)get<int>(n1);
+    else 
+        num1 = get<double>(n1);
+    if (holds_alternative<int>(n2)) 
+        num2 = (double)get<int>(n2);
+    else 
+        num2 = get<double>(n2);
+
+    if (op.size() > 1) {
+        result = pow(num1, num2);
+        return result;
+    }
+    switch (op[0]) {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        if (num2 != 0) 
+            result = num1 + num2;
+        else 
+            result = 0;
+        break;
+    }
+    return result;
+}
---
+++ think-lang/think/README.md
@@ -0,0 +1,21 @@
+# *Think* Language Development
+
+## Background/Idea
+
+This project was born out of the desire to have a very simple way to express logical ideas in a programming language.
+
+I don't necessarily dislike the existing logic programming languages, such as Prolog and CLIPS, but I found myself getting more frustrated with them than I wanted to be. So what's the obvious solution? To create my own, of course!
+
+## About
+
+*Think* is a general-purpose logic programming language built by someone who is (still) not a very experienced programmer, but who has always had a certain affinity for understanding and expressing logical concepts in the real world. Unlike my other language development project, *[Prog](../../prog-lang/README.md)*, this is **not** a learning project. Instead, I am viewing it as more of a chance for me to implement a tool that I don't see available in the current software landscape (which will happen to help me learn as I work on it... big difference).  
+
+## Name
+
+I am calling this project *Think*. In my eyes, a general-purpose logic programming language should be able to offload some of the brainpower in decision-making from you onto your machine. Of course, they have their more "academic" uses, but my primary objective is to make a language that anyone can learn in less than a day, which will help them reason about, and *think* through their problems. 
+
+## Design
+
+My goal is for this language to have a minimalist syntax which resembles a blend of high-level languages, like Python and Ruby, with a set of declarative terms which more closely resemble something like a SQL or LINQ query. 
+
+My other goal is for the language to be executable in the same way as an interpreted, general-purpose programming language. One of my biggest frustrations with Prolog is the need to either interact with your rules through REPL queries or by writing a main entry point and loading it into the shell. I just wanted to be able to write something that looks like a nice, simple script (Bash/Python/Ruby/etc.), run the language interpreter, and have it spit out a result at the end. Apparently, this was too much to ask for, so now I'm doing it myself. 
