ba0d37769749bd16ff67b5c49707606855d590d1
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-06-09 18:36:15 -0400

    Restart C version of the parser

---
+++ prog/advanced-parser/c/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "utils.h" 
+
+int main(void)
+{
+    char* file = getFileContents();
+    printf("%s\n", file);
+
+
+
+
+
+    return 0;
+}
---
+++ prog/advanced-parser/c/utils.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+char* getFileContents()
+{
+    FILE* fptr;
+    long length;
+
+    fptr = fopen("../../../test.pr", "r");
+    if (fptr == NULL) {
+        perror("Problem opening file.\n");
+        return NULL;
+    }
+
+    fseek(fptr, 0, SEEK_END);
+    length = ftell(fptr);
+    rewind(fptr);
+
+    char* buffer = (char*)malloc(sizeof(char) * length + 1);
+    if (buffer == NULL) {
+        perror("Problem creating buffer");
+        fclose(fptr);
+        return NULL;
+    }
+
+    if (fread(buffer, sizeof(char), length, fptr) != length) {
+        perror("Failed to read file.");
+        fclose(fptr);
+        free(buffer);
+        return NULL;
+    }
+
+    buffer[length] = '\0';
+    fclose(fptr);
+
+    return buffer;
+}
