78b56ec26a67a1ced42dd2599e7af63bab52cf89
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-07-11 13:17:51 -0400

    Update root dir

--- prog/experiments/c/main.c
+++ prog/experiments/c/main.c
@@ -4,18 +4,19 @@
 int main(void)
 {
     string* file = readFile("../../../test.pr");
+    puts(file->data);
     stringArray* lines = strSplit(file, "\n");
     string* preparedFile = prepareFile(lines);
     puts(preparedFile->data);
     strFree(preparedFile);
+    puts("Run leaks now...");
     sleep(10);
     return 0;
 }
 
 void comment_storage()
 {
-    // gcc main.c str.c utils.c -o main -fsanitize=address -g
-    // comment storage:
+    // gcc main.c str.c utils.c lexer.c -o main -fsanitize=address -g
     // todo: design data type storage
     // todo: design control flow 
     // todo: execute top level print statements
