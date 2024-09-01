2dd32bb0a62d6381fa6778a2b9874bf50af7710f
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-09-01 19:50:52 -0400

    Move c3 to past_experiments

--- prog/src/c/utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "str.h"
-#include "token.h"
-
-string* readFile(char* path);
-void lstrip(string* original);
-int indexOf(string* str, char c);
-int indexOfStr(string* str, char* s);
